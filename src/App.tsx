072e241d446d7d2e565081f8ddc594249cf8a0fa
MarcosAlexandre <82763928+Catalendas@users.noreply.github.com>
2022-06-01 20:02:46 -0300

    Projeto finalizado

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from './components/Button';
+import { MovieCard } from './components/MovieCard';
+
+ import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+   
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar selectedGenreId={selectedGenreId} handleClickButton={handleClickButton}/>
+      <Content selectedGenre={selectedGenre} selectedGenreId={selectedGenreId}/>
+    </div>
+  )
+}
